907611907ca505df7505bb2f185c2752afc46924
Henry Blanchette <henry@monkeyface.com>
2026-02-19 20:13:39 -0500

    better validation instructions

--- scripts/get-instructions-for-validation.ts
+++ scripts/get-instructions-for-validation.ts
@@ -6,11 +6,15 @@ console.log(
   `
 These are instructions for validating your demo implementation.
 
-To validate your demo implementation, use the ${cq("http.server")} Python module launch a web server at ${cq(path.join(public_dirpath, "<DEMO_SLUG>"))} (which is the directory where you implemented the demo) and use the Playwright library for Python to perform these steps:
+To validate your demo implementation, write Python script that uses the ${cq("http.server")} module and the Playwright library to do the following:
 
+- Start a web server at ${cq(path.join(public_dirpath, "<DEMO_SLUG>"))} (which is the directory where you implemented the demo).
 - Open a browser window to view the demo.
-- Interact with the demo to make sure the key aspects are working properly
+- Wait for the demo to load.
+- Interact with the demo to make sure the key aspects are working properly.
 - After some interaction, take a screenshot and save it as an image file at this path: ${cq(path.join(screenshots_dirpath, "<DEMO_SLUG>"))}.
+
+Run the Python script and inspect the results.
 
 If there are any issues with the demo, modify your implementation to address them.
 
